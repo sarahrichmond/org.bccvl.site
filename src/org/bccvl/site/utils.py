f0ea2c75514481aa76a27ed47d47ccffce0307ca
Yong Liaw <y.liaw@griffith.edu.au>
2015-08-05 12:48:04 +1000

    BCCVLB-122: Validate input file to check for valid coordinates and correct columns number of columns.

--- src/org/bccvl/site/utils.py
+++ src/org/bccvl/site/utils.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 import json
 import re
+import csv
 
 class DecimalJSONEncoder(json.JSONEncoder):
 
@@ -55,3 +56,39 @@ class Period(object):
         if self.scheme:
             parts.append("scheme=%s;" % self.scheme)
         return u' '.join(parts)
+        
+"""
+Validate the coordinates in the CSV file, and return a list of issues found if any.
+"""
+def ValidateData(csvFile, longitudeName = 'lon', latitudeName = 'lat'):
+    csv_reader = csv.DictReader(csvFile, skipinitialspace = True)
+    csv_reader.fieldnames = [f.strip() for f in csv_reader.fieldnames]
+    headers = csv_reader.fieldnames
+    issues = []
+    line = 1
+    err_fmt = "Line %d: %s"
+        
+    # check for longitude and latitude columns
+    if not longitudeName in headers or not latitudeName in headers:
+        # Don't have the coordinate columns
+        issues.append(err_fmt %(line, "Missing 'lon' or/and 'lat' column(s)"))
+        return issues
+         
+    # Validate the number of columns and coordinates
+    num_cols = len(headers)
+    for row in csv_reader:
+        line += 1       
+        # check that the row has the correct number of columns
+        if len(row) != num_cols:
+            msg = "Incorrect number of columns (%d columns found but %d expected)" %(len(row), num_cols)
+            issues.append(err_fmt %(line, msg))
+            continue
+        try:
+            # check that the coordinates are valid numbers
+            lng_f = float(row[longitudeName])
+            lat_f = float(row[latitudeName])
+        except:
+            msg = "Invalid coordinate data (lat='%s', lon='%s')" %(row[latitudeName], row[longitudeName])
+            issues.append(err_fmt %(line, msg))
+            continue
+    return issues
